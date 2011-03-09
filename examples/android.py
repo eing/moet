d1f5539ce10384f310ddc70edb5072aa3b7ba6e0
moet <projectmoet@gmail.com>
2011-03-08 18:51:59 -0800

    android device update

--- examples/android.py
+++ examples/android.py
@@ -15,7 +15,6 @@ import applib
 import appbase
 from androidlib import *
 
-
 class AndroidImpl(appbase.AppInterface):
     """
         Android implementation of methods specified in interface class.
@@ -29,79 +28,40 @@ class AndroidImpl(appbase.AppInterface):
 
     def launch(self):
         """ Launch app """
-        touch(128,388)
+        launch('com.android.contacts/.DialtactsContactsEntryActivity -a android.intent.action.MAIN')
 
     def screen(self, name="phone"):
         """ Navigate to different screens
             @param name phone, log, contacts, fav
         """
-        if name == "phone":
-            touch(17,44)
-        elif name == "contacts":
-            touch(210, 53)
-        elif name == "fav":
-            touch(277, 52)
-        else:
-            touch(140, 49)
-
-    def phone(self, phone, options="[end]"):
-        """ Phone activity
-            @param phone phone number to dial
-            @param options list of activities after call is placed
-        """
-        phone = str(phone)
-        self.screen("phone")
-        phonelen = len(phone)
-        index = 0 
-        while index < phonelen:
-            dialnum = phone[index]
-            if dialnum == '1':  
-                touch(81,209)
-            elif dialnum == '2':  
-                touch(140,208)
-            elif dialnum == '3':  
-                touch(239,198)
-            elif dialnum == '4':  
-                touch(53,261)
-            elif dialnum == '5':  
-                touch(166,266)
-            elif dialnum == '6':  
-                touch(242,274)
-            elif dialnum == '7':  
-                touch(33,329)
-            elif dialnum == '8':  
-                touch(147,322)
-            elif dialnum == '9':  
-                touch(245,327)
-            elif dialnum == '0':  
-                touch(147,383)
-            index = index + 1
-            
-        # dial  
-        touch(125,467)
-        time.sleep(2)
-        
-        # end call
-        self.endcall()
-
-    def endcall(self):
-        """ assumes you are in contact detail screen """
-        menu()
+        scroll('up', 2)
         time.sleep(1)
-        touch(275,391)
-
+        if name == "phone":
+            scroll('left', 3)
+        elif name == "contacts":
+            scroll('right', 4)
+            scroll('left', 1)
+        elif name == "fav":
+            scroll('right', 4)
+        else:
+            scroll('left', 4)
+            scroll('right',1 )
+        time.sleep(1)
 
     def add(self, contact, options=["done"]):  
         """ Add contact """  
         self.screen("contacts")
         menu()
         # click add contact
-        touch(242,375)
+        scroll('up', 2)
+        time.sleep(1)
+        scroll('right')
+        time.sleep(1)
+        enter()
         time.sleep(1)
 
         # add given name
-        scroll('up')
-        scroll('up')
+        scroll('up', 7)
         scroll('down')
         enter(contact.getFirstname())
         scroll('down')
@@ -109,25 +69,27 @@ class AndroidImpl(appbase.AppInterface):
 
         # add family name
         enter(contact.getLastname())
-        scroll('down')
-        scroll('down')
+        scroll('down', 2)
         time.sleep(1)
 
         # add phone
         enter(contact.getPhone())
-        scroll('down')
-        scroll('down')
+        scroll('down', 2)
         time.sleep(1)
       
         # add email
         enter(contact.getEmail())
         time.sleep(1)
         
+        scroll('down', 7)
+        time.sleep(1)
+        scroll('right')
         # save or revert contact
         if options[0] == 'revert':
-            touch(211,453)
+            scroll('right')
         else:
-            touch(94,462)
+            scroll('left')
+        enter()
         time.sleep(3)
 
 
@@ -136,7 +98,8 @@ class AndroidImpl(appbase.AppInterface):
         menu()
         time.sleep(1)
         # click search
-        touch(60,361)
+        scroll('up', 2)
+        enter()
         time.sleep(1)
 
         # enter search term 
@@ -160,11 +123,15 @@ class AndroidImpl(appbase.AppInterface):
         time.sleep(1)
 
         # click delete contact
-        touch(265,437)
+        scroll('down', 2)
+        time.sleep(1)
+        scroll('right', 1)
+        time.sleep(1)
+        enter()
         time.sleep(1)
 
         # click Yes on delete prompt
-        touch(89,301)
+        enter() 
         time.sleep(2)
 
         # go back to main screen
